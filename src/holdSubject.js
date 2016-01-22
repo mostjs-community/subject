47e0eb61b71e6c8173f5d9ed8fbcb3d4773dadb0
TylorS <tlsteinberger167@gmail.com>
2016-01-22 10:37:12 -0500

    feat(): rework api
    Create 2 functions `subject()` and `holdSubject()` to cover
    all issues.
    
    Update the README with holdSubject() example
    Add API documentation

--- src/holdSubject.js
+++
@@ -1,8 +0,0 @@
-import subject from './subject'
-import hold from '@most/hold'
-
-export default function holdSubject() {
-  const {sink, stream} = subject()
-  const holdStream = hold(stream)
-  return {sink, stream: holdStream}
-}
--- test/index.js
+++ test/index.js
@@ -1,195 +1,181 @@
 import assert from 'assert'
 import {Stream} from 'most'
-import hold from '@most/hold'
-import * as Subjects from '../src'
+import {subject, holdSubject} from '../src'
 
-Object.keys(Subjects)
-  .forEach(subject => {
-    describe(`${subject}`, () => {
-      it('should return Object with stream and sink', done => {
-        const s = Subjects[subject]()
-        assert.strictEqual(typeof s, 'object')
-        assert.strictEqual(typeof s.stream, 'object')
-        assert.strictEqual(typeof s.sink, 'object')
-        done()
-      })
+describe('subject()', () => {
+  it('should return Object with stream and sink', done => {
+    const s = subject()
+    assert.strictEqual(typeof s, 'object')
+    assert.strictEqual(typeof s.stream, 'object')
+    assert.strictEqual(typeof s.sink, 'object')
+    done()
+  })
 
-      describe('stream', () => {
-        it('should be an extension of Stream', done => {
-          const {stream} = Subjects[subject]()
-          assert.strictEqual(stream instanceof Stream, true)
+  it('should have alias `observer` for `sink`', done => {
+    const s = subject()
+    assert.strictEqual(typeof s.observer, 'object')
+    assert.strictEqual(typeof s.observer, 'object')
+    assert.strictEqual(s.observer, s.observer)
+    done()
+  })
+
+  describe('stream', () => {
+    it('should be an extension of Stream', done => {
+      const {stream} = subject()
+      assert.strictEqual(stream instanceof Stream, true)
+      done()
+    })
+
+    it('should inherit Stream combinators', done => {
+      const {observer, stream} = subject()
+
+      stream
+        .map(x => x * x)
+        .forEach(x => {
+          assert.strictEqual(x, 25)
           done()
         })
 
-        it('should be hold-able', done => {
-          const {sink, stream} = Subjects[subject]()
-          const hstream = hold(stream)
-
-          hstream
-            .forEach(x => {
-              assert.strictEqual(x, 1)
-            })
-
-          setTimeout(() => {
-            hstream.forEach(x => {
-              assert.strictEqual(x, 1)
-              done()
-            })
-          }, 10)
-
-          sink.add(1)
-        })
-
-        it('should inherit Stream combinators', done => {
-          const {sink, stream} = Subjects[subject]()
-
-          stream
-            .map(x => x * x)
-            .forEach(x => {
-              assert.strictEqual(x, 25)
-              done()
-            })
-
-          sink.add(5)
-          sink.end()
-        })
-      })
-
-      describe('sink', () => {
-        it('should allow nexting events', done => {
-          const {sink, stream} = Subjects[subject]()
-
-          assert.strictEqual(typeof sink.add, 'function')
-
-          stream.forEach(x => {
-            assert.strictEqual(x, 1)
-            done()
-          })
-
-          sink.add(1)
-          sink.end()
-        })
-
-        it('should allow sending errors' , done => {
-          const {sink, stream} = Subjects[subject]()
-
-          assert.strictEqual(typeof sink.error, 'function')
-          stream
-            .drain()
-            .then(assert.fail)
-            .catch(err => {
-              assert.strictEqual(err.message, 'Error Message')
-              done()
-            })
-
-          sink.add(1)
-          sink.add(2)
-          sink.error(new Error('Error Message'))
-        })
-
-        it('should allow ending of stream', done => {
-          const {sink, stream} = Subjects[subject]()
-
-          stream
-            .forEach(assert.fail)
-            .then(done)
-            .catch(assert.fail)
-
-          sink.end()
-        })
-
-        it('should not allow events after end', done => {
-          const {sink, stream} = Subjects[subject]()
-
-          const now = () => setTimeout(done, 10)
-          stream
-            .forEach(assert.fail)
-            .then(now)
-            .catch(assert.fail)
-
-          sink.end()
-          sink.add(1)
-        })
-
-        it('sink should have es7 observer methods', done => {
-          const {sink, stream} = Subjects[subject]()
-
-          assert.strictEqual(typeof sink.add, 'function')
-
-          stream.forEach(x => {
-            assert.strictEqual(x, 1)
-          }).then(done)
-
-          sink.next(1)
-          sink.complete()
-        })
-      })
-
-      if (subject === 'subject') {
-        it('should be `hot` by default', done => {
-          const {sink, stream} = Subjects[subject]()
-
-          sink.add(2)
-          sink.add(3)
-
-          stream
-            .observe(x => {
-              assert.strictEqual(x, 1)
-            })
-            .then(done)
-            .catch(assert.fail)
-
-          sink.add(1)
-          sink.end()
-        })
-      }
-
-      if(subject === 'holdSubject') {
-        describe('holdSubject', () => {
-          it('should be held by default', done => {
-            const {sink, stream} = Subjects[subject]()
-
-            stream
-              .forEach(x => {
-                assert.strictEqual(x, 1)
-              })
-
-            setTimeout(() => {
-              stream.forEach(x => {
-                assert.strictEqual(x, 1)
-                done()
-              })
-            }, 10)
-
-            sink.add(1)
-          })
-        })
-      }
-
-      if (subject === 'behaviorSubject') {
-        describe('behaviorSubject', () => {
-          it('should allow for a default value', done => {
-            const {sink, stream} = Subjects[subject](123)
-
-            stream.forEach(x => {
-              assert.strictEqual(x, 123)
-            }).then(done)
-
-            sink.end()
-          })
-
-          it('should allow finding latest value from `.value`', done => {
-            const {sink, stream} = Subjects[subject](123)
-
-            stream.forEach(x => {
-              assert.strictEqual(x, sink.value)
-            }).then(done)
-
-            sink.add(1)
-            sink.add(2)
-            sink.end()
-          })
-        })
-      }
+      observer.next(5)
+      observer.complete()
     })
   })
+
+  describe('observer', () => {
+    it('should have add and next for sending new values', done => {
+      const {observer} = subject()
+
+      assert.strictEqual(typeof observer.add, 'function')
+      assert.strictEqual(typeof observer.next, 'function')
+
+      done()
+    })
+
+    it('should allow nexting events', done => {
+      const {observer, stream} = subject()
+
+      assert.strictEqual(typeof observer.next, 'function')
+
+      stream.forEach(x => {
+        assert.strictEqual(x, 1)
+        done()
+      })
+
+      observer.next(1)
+      observer.complete()
+    })
+
+    it('should allow sending errors' , done => {
+      const {observer, stream} = subject()
+
+      assert.strictEqual(typeof observer.error, 'function')
+      stream
+        .drain()
+        .then(assert.fail)
+        .catch(err => {
+          assert.strictEqual(err.message, 'Error Message')
+          done()
+        })
+
+      observer.next(1)
+      observer.next(2)
+      observer.error(new Error('Error Message'))
+    })
+
+    it('should have end and complete for ending stream', done => {
+      const {observer} = subject()
+
+      assert.strictEqual(typeof observer.end, 'function')
+      assert.strictEqual(typeof observer.complete, 'function')
+
+      done()
+    })
+
+    it('should allow ending of stream', done => {
+      const {observer, stream} = subject()
+
+      stream
+        .forEach(assert.fail)
+        .then(done)
+        .catch(assert.fail)
+
+      observer.complete()
+    })
+
+    it('should not allow events after end', done => {
+      const {observer, stream} = subject()
+
+      const now = () => setTimeout(done, 10)
+      stream
+        .forEach(assert.fail)
+        .then(now)
+        .catch(assert.fail)
+
+      observer.complete()
+      observer.next(1)
+    })
+  })
+
+  it('should allow starting with an initialValue', done => {
+    const {observer, stream} = subject(1)
+
+    stream.observe(x => {
+      assert.strictEqual(x, 1)
+    })
+
+    stream.observe(x => {
+      assert.strictEqual(x, 1)
+    })
+
+    observer.complete()
+    setTimeout(done, 10)
+  })
+})
+
+describe('holdSubject', () => {
+  it('should replay the last value', done => {
+    const {observer, stream} = holdSubject()
+    observer.next(1)
+    observer.next(2)
+
+    stream.forEach(x => {
+      assert.strictEqual(x, 2)
+    }).then(done)
+
+    setTimeout(observer.complete, 10)
+  })
+
+  it('should allow for adjusting bufferSize of stream', done => {
+    const {observer, stream} = holdSubject(3)
+
+    observer.next(1)
+    observer.next(2)
+    observer.next(3)
+    observer.next(4)
+
+    stream
+      .reduce((x, y) => x.concat(y), [])
+      .then(x => {
+        assert.deepEqual(x, [2, 3, 4])
+        setTimeout(done, 25)
+      })
+
+    observer.complete()
+  })
+
+  it('should allow for adding an initialValue', done => {
+    const {observer, stream} = holdSubject(4, 1)
+
+    observer.next(2)
+    observer.next(3)
+    observer.next(4)
+
+    stream.reduce((x, y) => x.concat(y), [])
+      .then(x => {
+        assert.deepEqual(x, [1, 2, 3, 4])
+        setTimeout(done, 25)
+      })
+
+    observer.complete()
+  })
+})
