d175d908f578d3a693496ca901d7bacb1f7c6492
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-09-25 12:12:57 -0400

    feat: default to id function

--- src/create.ts
+++ src/create.ts
@@ -1,14 +1,15 @@
 import { AttachSink } from './types'
 import { ProxyStream } from './ProxyStream'
 import { Stream } from '@most/types'
+import { id } from '@most/prelude'
 
 export function create<A = any>(): [AttachSink<A>, Stream<A>]
 export function create<A, B>(f: (stream: Stream<A>) => B): [AttachSink<A>, B]
 
 export function create<A, B = Stream<A>>(
-  f?: (stream: Stream<A>) => B
+  f: (stream: Stream<A>) => B = id as (stream: Stream<A>) => B
 ): [AttachSink<A>, B] {
   const source = new ProxyStream()
 
-  return [source, f === void 0 ? (source as any) as B : f(source)]
+  return [source, f(source)]
 }
