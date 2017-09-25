a9fc6930a6a8dce69043887d881e4d3b1ddc3a78
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-09-25 12:30:48 -0400

    feat: add Subject<A, B> type

--- src/create.ts
+++ src/create.ts
@@ -1,14 +1,14 @@
-import { AttachSink } from './types'
 import { ProxyStream } from './ProxyStream'
 import { Stream } from '@most/types'
+import { Subject } from './types'
 import { id } from '@most/prelude'
 
-export function create<A = any>(): [AttachSink<A>, Stream<A>]
-export function create<A, B>(f: (stream: Stream<A>) => B): [AttachSink<A>, B]
+export function create<A = any>(): Subject<A, A>
+export function create<A, B>(f: (stream: Stream<A>) => Stream<B>): Subject<A, B>
 
 export function create<A, B = A>(
   f: (stream: Stream<A>) => Stream<B> = id as (stream: Stream<A>) => Stream<B>
-): [AttachSink<A>, Stream<B>] {
+): Subject<A, B> {
   const source = new ProxyStream<A>()
 
   return [source, f(source)]
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,7 @@
 import { Sink, Stream } from '@most/types'
 
+export type Subject<A, B> = [Sink<A>, Stream<B>]
+
 export interface AttachSink<A> extends Sink<A> {
   attach(source: Stream<A>): Stream<A>
 }
