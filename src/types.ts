cc52fbb94c19521073c35e674dc15630eda5259c
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-09-25 12:43:16 -0400

    fix: fix Subject type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 import { Sink, Stream } from '@most/types'
 
-export type Subject<A, B> = [Sink<A>, Stream<B>]
+export type Subject<A, B> = [AttachSink<A>, Stream<B>]
 
 export interface AttachSink<A> extends Sink<A> {
   attach(source: Stream<A>): Stream<A>
